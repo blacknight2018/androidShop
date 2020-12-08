479194808fb00d1c0939034eb5ab5ffd9a96474e
code <1462063314@qq.com>
2020-12-08 20:35:46 +0800

    添加了登录界面

---
+++ app/src/main/java/com/example/fuck2/ui/login/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.fuck2.ui.login;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fuck2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+
+    }
+
+}

e34c3c112cea43bb83d978c3a56ca43908629507
code <1462063314@qq.com>
2020-12-12 00:19:59 +0800

    添加了动态添加规格radiobutton的代码

--- app/src/main/java/com/example/fuck2/SelectSpecPopWin.java
+++ app/src/main/java/com/example/fuck2/SelectSpecPopWin.java
@@ -2,37 +2,57 @@ package com.example.fuck2;
 
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+
+import com.example.fuck2.config.Config;
+import com.example.fuck2.ui.PopWinSelectSpecItem;
+import com.example.fuck2.utils.ApiThread;
+import com.example.fuck2.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SelectSpecPopWin extends PopupWindow {
     private Context mContext;
 
     private View view;
-
+    private int subGoodsId = 3;
+    private MHandler mHandler = new MHandler();
     private Button btn_take_photo, btn_pick_photo, btn_cancel;
+
+    private class MHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            int code = msg.what;
+            if (0 == code) {
+                String body = msg.obj.toString();
+            }
+        }
+    }
+
+    private void addOneSpec(String title, List<String> values) {
+        LinearLayout linearLayout = view.findViewById(R.id.spec_set);
+        PopWinSelectSpecItem popWinSelectSpecItem = new PopWinSelectSpecItem(mContext);
+        popWinSelectSpecItem.setTitle(title);
+        popWinSelectSpecItem.setValues(values);
+        linearLayout.addView(popWinSelectSpecItem);
+    }
 
     public SelectSpecPopWin(Context mContext, View.OnClickListener itemsOnClick) {
         this.view = LayoutInflater.from(mContext).inflate(R.layout.popwin_select_spec, null);
-
-//        btn_take_photo = (Button) view.findViewById(R.id.btn_take_photo);
-//        btn_pick_photo = (Button) view.findViewById(R.id.btn_pick_photo);
-//        btn_cancel = (Button) view.findViewById(R.id.btn_cancel);
-//        // 取消按钮
-//        btn_cancel.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View v) {
-//                // 销毁弹出框
-//                dismiss();
-//            }
-//        });
-//        // 设置按钮监听
-//        btn_pick_photo.setOnClickListener(itemsOnClick);
-//        btn_take_photo.setOnClickListener(itemsOnClick);
+        this.mContext = mContext;
 
         // 设置外部可点击
         this.setOutsideTouchable(true);
@@ -53,15 +73,10 @@ public class SelectSpecPopWin extends PopupWindow {
             }
         });
 
-
-        /* 设置弹出窗口特征 */
-        // 设置视图
         this.setContentView(this.view);
-        // 设置弹出窗体的宽和高
         this.setHeight(RelativeLayout.LayoutParams.MATCH_PARENT);
         this.setWidth(RelativeLayout.LayoutParams.MATCH_PARENT);
 
-        // 设置弹出窗体可点击
         this.setFocusable(true);
 
         // 实例化一个ColorDrawable颜色为半透明
@@ -71,5 +86,22 @@ public class SelectSpecPopWin extends PopupWindow {
 
         // 设置弹出窗体显示时的动画，从底部向上弹出
         this.setAnimationStyle(R.style.select_spec_anim);
+
+        HashMap<String, String> param = new HashMap<>();
+        param.put("sub_goods_id", String.valueOf(subGoodsId));
+        new ApiThread(0, mHandler, "get", Config.getServerAddress() + "/v1/goods", Utils.MapToHttpParam(param)).start();
+
+        List<String> clothSizes = new ArrayList<>();
+        clothSizes.add("1");
+        clothSizes.add("2");
+        clothSizes.add("3");
+        addOneSpec("大小", clothSizes);
+
+        List<String> clothColor = new ArrayList<>();
+        clothColor.add("蓝色");
+        clothColor.add("黑色");
+        clothColor.add("绿色");
+        addOneSpec("颜色", clothColor);
+
     }
 }
---
+++ app/src/main/java/com/example/fuck2/ui/PopWinSelectSpecItem.java
@@ -0,0 +1,66 @@
+package com.example.fuck2.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.fuck2.R;
+
+import java.util.List;
+
+
+public class PopWinSelectSpecItem extends LinearLayout {
+    private String title;
+    private List<String> values;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        TextView titleTextView = findViewById(R.id.title);
+        titleTextView.setText(title);
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public void setValues(List<String> values) {
+        this.values = values;
+        RadioGroup radioGroup = findViewById(R.id.spec_set);
+        for (int i = 0; i < values.size(); i++) {
+            RadioButton radioButton = (RadioButton) LayoutInflater.from(getContext()).inflate(R.layout.spec_btn, null);
+            radioButton.setText(values.get(i));
+            radioButton.getLayoutParams();
+            radioGroup.addView(radioButton);
+        }
+    }
+
+    public PopWinSelectSpecItem(Context context) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.popwin_select_spec_item, this, true);
+    }
+
+    public PopWinSelectSpecItem(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.popwin_select_spec_item, this, true);
+    }
+
+    public PopWinSelectSpecItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        LayoutInflater.from(context).inflate(R.layout.popwin_select_spec_item, this, true);
+    }
+
+    public PopWinSelectSpecItem(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        LayoutInflater.from(context).inflate(R.layout.popwin_select_spec_item, this, true);
+    }
+}
